c275161e605cd237df0e7fed27520a3f8f64dd28
remo0619 <ichinohe@aomori-innovators.com>
2026-02-06 15:50:27 +0900

    Create/TimeZone

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all #allメソッドはusersテーブルのレコードを全て取得できるメソッド
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :email, :age, :introduction))
+    if @user.save
+      flash[:notice] = "ユーザーを新規登録しました"
+      redirect_to :users
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
